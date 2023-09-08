cb5b0747c0e52c7eff1239f0b271922dcc2adf08
TathagatTulsi <iamtathagat.tulsi@gmail.com>
2023-09-08 18:37:26 +0530

    update sept8

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 // const mongo_uri = "mongodb+srv://Tulsi_User:Tulsi_User@cluster0.0vi613m.mongodb.net/crud?retryWrites=true&w=majority"
 const mongo_uri = "mongodb://localhost:27017/saas"
-//  const mongo_uri = "mongodb+srv://Tulsi_User:<password>@cluster0.0vi613m.mongodb.net/"
+// const mongo_uri = "mongodb+srv://Tulsi_User:Tulsi_User@cluster0.0vi613m.mongodb.net/"
 mongoose.connect(mongo_uri , {
     useNewUrlParser:true , 
     useUnifiedTopology:true})
